a195f881653b3f16284a130f4bfe1f23f8ba8e1d
alexoreiro <alexis.oreiro@icloud.com>
2020-12-21 12:22:04 +0000

    library and monty file added.

---
+++ monty.c
@@ -0,0 +1,43 @@
+#include "monty.h"
+
+glob_t glob = {NULL, NULL, NULL, 0};
+/**
+ * free_stack - free all malloc'ed memory of the stack
+ * @stack: pointer to pointer to stack
+ */
+void free_stack(stack_t **stack)
+{
+stack_t *tmp1, *tmp2;
+
+tmp1 = *stack;
+while (tmp1)
+{
+tmp2 = tmp1->next;
+free(tmp1);
+tmp1 = tmp2;
+}
+if (glob.buf != NULL)
+free(glob.buf);
+fclose(glob.fp);
+}
+/**
+ * main - interpret monty byte code files
+ * @argc: argument count
+ * @argv: array of strings containing the arguments
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(int argc, char *argv[])
+{
+char *filename;
+stack_t *head = NULL;
+
+if (argc != 2)
+{
+dprintf(STDERR_FILENO, "USAGE: monty file\n");
+exit(EXIT_FAILURE);
+}
+filename = argv[1];
+read_file(filename, &head);
+free_stack(&head);
+return (EXIT_SUCCESS);
+}
---
+++ monty.h
@@ -0,0 +1,91 @@
+#ifndef MONTY_H
+#define MONTY_H
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#define DELIMS "\t\n "
+#define TRUE (1 == 1)
+#define FALSE (!TRUE)
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+  int n;
+  struct stack_s *prev;
+  struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+  char *opcode;
+  void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct glob_s - global variable
+ * @fp: pointer to file
+ * @stack: pointer to top of stack
+ * @buf: line buffer storing getline string
+ */
+typedef struct glob_s
+{
+  FILE *fp;
+  stack_t **stack;
+  char *buf;
+  int queue;
+} glob_t;
+
+extern glob_t glob;
+
+/* monty.c */
+void free_stack(stack_t **stack);
+
+/* process_file.c */
+void read_file(const char *filename, stack_t **stack);
+void execute_op(stack_t **stack, char *op, unsigned int line_number);
+int only_digit(char *num);
+void _push_stack(stack_t **stack, unsigned int line_number);
+void _push_queue(stack_t **stack, unsigned int line_number);
+
+/* opcodes_1.c */
+void _pall(stack_t **stack, unsigned int line_number);
+void _push(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+
+/* opcodes_2.c */
+void _add(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
+void _sub(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+
+/* opcodes_3.c */
+void _mod(stack_t **stack, unsigned int line_number);
+void _pchar(stack_t **stack, unsigned int line_number);
+void _pstr(stack_t **stack, unsigned int line_number);
+void _rotl(stack_t **stack, unsigned int line_number);
+void _rotr(stack_t **stack, unsigned int line_number);
+
+#endif
